dd934d239c51a6d107305cdf5bbdce7c7976f536
Carlos Iriarte <ciriarte@gmail.com>
2008-05-14 00:51:36 -0500

    First commit. Added base structure and home controller.

---
+++ controllers/home.py
@@ -0,0 +1,27 @@
+import cgi
+import wsgiref.handlers
+import os
+
+from google.appengine.ext        import db
+from google.appengine.api        import users
+from google.appengine.ext        import webapp
+from google.appengine.ext.webapp import template
+
+class HomeController(webapp.RequestHandler):
+  def get(self):
+	path = os.path.join(os.path.dirname(__file__), '../views/index.html')
+	
+	template_values = {
+		
+	}
+	
+	self.response.out.write(template.render(path, template_values))
+
+def main():
+  application = webapp.WSGIApplication(
+                                       [('/', HomeController)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+  main()
