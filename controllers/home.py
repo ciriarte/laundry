aafcc84c6e0d2fc8e64ac5aba59bac2222719477
Carlos Iriarte <ciriarte@gmail.com>
2008-05-15 00:56:41 -0500

    Login handler added. New css for rounded buttons added.

--- controllers/home.py
+++ controllers/home.py
@@ -9,18 +9,32 @@ from google.appengine.ext.webapp import template
 
 class HomeController(webapp.RequestHandler):
   def get(self):
-	path = os.path.join(os.path.dirname(__file__), '../views/home.html')
+	path = os.path.join(os.path.dirname(__file__),
+	                    '../views/%s.html' % ("home"
+	                    if self.request.path == "/" else self.request.path[1:]))
 	
 	template_values = {
-		
-	}
+      'section': self.request.path[1:],
+    }
 	
 	self.response.out.write(template.render(path, template_values))
 
+class LoginController(webapp.RequestHandler):
+	def get(self):
+	    user = users.get_current_user()
+	    if user:
+	      self.redirect(user.nickname() + "/dashboard")
+	    else:
+	      self.redirect(users.create_login_url(self.request.uri))
+
 def main():
   application = webapp.WSGIApplication(
-                                       [('/', HomeController)],
-                                       debug=True)
+                                       [('/',         HomeController),
+                                        ('/home',     HomeController),
+										('/services', HomeController),
+										('/contact',  HomeController),
+										('/login',    LoginController)],
+                                       debug=False)
   wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
